5ec6ee9b885d3304feca304dc872157a33101b76
Saksham <chauhanssaksham@gmail.com>
2020-05-02 03:37:37 +0530

    Add CartItem component

---
+++ src/components/CartLayout/CartItem.tsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class CartItem extends Component{
+    render(){
+        return (
+            <div className="cart-item">
+                <div className="left-block">
+                    <img alt="" style={styles.image}/>
+                </div>
+                <div className="right-block">
+                    <div style={{ fontSize:25 }}>Phone</div>
+                    <div style={{ color:'#777' }}>Rs 999</div>
+                    <div style={{ color:'#777' }}>Qty: 1</div>
+                    <div className="card-item-actions">
+                        {/* Buttons */}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const styles: any = {
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        background: '#ccc'
+    }
+}
+
+export default CartItem;
