321ae4f03922959e815d25e05b22a50bb16a432c
Saksham <chauhanssaksham@gmail.com>
2020-05-02 08:17:50 +0530

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Cart from './components/CartLayout/Cart'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
+        <Navbar/>
         <Cart/>
     </div>
   );
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+interface Props{
+
+}
+
+interface State{
+
+}
+
+
+
+class Navbar extends Component<Props, State>{
+
+    render(){
+        return (
+            <div style={styles.nav}>
+                <div style={styles.cartIconContainer}>
+                    <img style={styles.cartIcon} src="https://image.flaticon.com/icons/svg/1170/1170627.svg" alt="cart-icon"/>
+                    <span style={styles.cartCount}>3</span>
+                </div>
+            </div>
+        )
+    }
+}
+
+const styles: any = {
+    cartIcon: {
+        height: 32,
+        marginRight: 20
+    },
+    nav: {
+        height: 70,
+        background: '#4267b2',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    cartIconContainer: {
+        position: 'relative'
+    },
+    cartCount: {
+        background: 'yellow',
+        borderRadius: '50%',
+        padding: '4px 8px',
+        position: 'absolute',
+        right: 0,
+        top: -9
+    }
+}
+
+export default Navbar;
